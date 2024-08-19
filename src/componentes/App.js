2cfe6e26b5037a4990818feba053ba9628fcc9f5
DESKTOP-8QBHFC5\SSL <simion.s.laurentiu@gmail.com>
2024-08-19 19:18:04 +0300

    added UseEffect

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -5,8 +5,17 @@ import "./Cart.css";
 import Filters from "./Filters";
 import Products from "./Products";
 import Cart from "./Cart";
+import {useEffect} from "react";
 // "https://fakestoreapi.com/products/"
 export default function App() {
+useEffect(()=>{
+  fetch("https://fakestoreapi.com/products"). then(async(response)=>
+  {
+    const productResponse = await response.json();
+    console.log(productResponse);
+  });
+
+}, []);
   return (
     <>
       <div className="wrapper">
