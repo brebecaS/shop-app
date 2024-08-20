1e339c8c20334dec6691bc1ed45e46c4d37a0d55
brebecaS <birleanu0rebeca@gmail.com>
2024-08-20 19:24:39 +0300

    show all if category is empty

--- src/componentes/Products.js
+++ src/componentes/Products.js
@@ -1,10 +1,12 @@
 import Product from "./Product";
 
 export default function Products({ productsList, category }) {
-  const filteredProducts = productsList.filter(
-    (product) => product.category === category
-  );
-  console.log(filteredProducts);
+  const filteredProducts = productsList.filter((product) => {
+    if (!category) return true;
+
+    if (product.category === category) return true;
+    else return false;
+  });
 
   const products = filteredProducts.map((product) => {
     return <Product product={product} key={product.id} />;
