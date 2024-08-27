dd4112ee6f9bcbf0902db3291835174f9d4d3c2f
Constantin Stoica <161923684+Stoica-Constantin27@users.noreply.github.com>
2024-08-27 20:43:57 +0300

    load products from Api

--- src/componentes/ProductPage.js
+++ src/componentes/ProductPage.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const ProductPage = () => {
   //1. accesam id-ul produsului
   //2. facem fetch catre server/api pentru a prelua datele produsului
   //3. afisam datele produsului
-  return <h1>Product page</h1>;
+
+  const params = useParams();
+  const productId = params.productId;
+  const [products, setProducts] = useState([]);
+  const currentProducts = products.find(
+    (product) => product.id === Number(productId)
+  );
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/").then(async (response) => {
+      const productsResponse = await response.json();
+      setProducts(productsResponse);
+    });
+  }, []);
+
+  console.log(products);
+  return <h1>{currentProducts?.title}</h1>;
 };
 
 export default ProductPage;
